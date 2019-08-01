b45c52ac482a924d5996f33f3c0c56b23415ce10
milerius <rmscastle@gmail.com>
2019-08-01 11:38:25 +0200

    fix(windows): revert operator

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -79,17 +79,7 @@ namespace antara::mmbot
 
     bool price_config::operator==(const price_config &rhs) const
     {
-#ifdef _MSC_VER
-        if (price_api_key.has_value() && rhs.price_api_key.has_value()) {
-             return price_endpoint.value() == rhs.price_endpoint.value()
-                    && price_api_key.value().value() == rhs.price_api_key.value().value();
-        } else {
-            return price_endpoint.value() == rhs.price_endpoint.value();
-        }
-#else
-        return price_endpoint.value() == rhs.price_endpoint.value()
-               && price_api_key == rhs.price_api_key;
-#endif
+        return price_endpoint.value() == rhs.price_endpoint.value();
     }
 
     bool price_config::operator!=(const price_config &rhs) const
