79121bcc6fb6eccceeaf6410ee8b4f8938ce3049
Daniel Revere <dcprevere@gmail.com>
2019-08-01 17:48:06 +0700

    feat: add placeholder tests for the order manager

---
+++ src/order_manager/order.manager.tests.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+ * Copyright © 2013-2019 The Komodo Platform Developers.                      *
+ *                                                                            *
+ * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * Komodo Platform software, including this file may be copied, modified,     *
+ * propagated or distributed except according to the terms contained in the   *
+ * LICENSE file                                                               *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+#include <doctest/doctest.h>
+
+#include <utils/mmbot_strong_types.hpp>
+#include "order.manager.hpp"
+
+namespace antara::mmbot::tests
+{
+    TEST_CASE ("individual orders can be placed on the cex")
+    {
+        CHECK(true);
+    }
+
+    TEST_CASE ("whole order sets can be placed")
+    {
+        CHECK(true);
+    }
+
+    TEST_CASE ("order status can be changed")
+    {
+        CHECK(true);
+    }
+}
