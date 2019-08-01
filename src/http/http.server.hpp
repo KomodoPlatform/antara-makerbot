9c3560493f61451319b2456615185a553eb5307e
milerius <rmscastle@gmail.com>
2019-08-01 19:24:15 +0200

    feat(http): add basic unit tests for the server skeleton

--- src/http/http.server.hpp
+++ src/http/http.server.hpp
@@ -19,6 +19,7 @@
 #include <memory>
 #include <restinio/all.hpp>
 #include <config/config.hpp>
+#include <restclient-cpp/restclient.h>
 
 namespace antara::mmbot
 {
@@ -34,15 +35,68 @@ namespace antara::mmbot
 
         http_server(const mmbot::config &mmbot_cfg) : mmbot_cfg_(mmbot_cfg)
         {
+            VLOG_SCOPE_F(loguru::Verbosity_INFO, pretty_function);
+        }
 
+        router create_routes()
+        {
+            VLOG_SCOPE_F(loguru::Verbosity_INFO, pretty_function);
+            using namespace restinio;
+            auto http_router = std::make_unique<restinio::router::express_router_t<>>();
+            http_router->http_get("/", [](auto req, auto) {
+                return req->create_response(status_ok()).set_body("Welcome.").done();
+            });
+
+            http_router->non_matched_request_handler(
+                    [](auto req) {
+                        return req->create_response(status_not_found()).set_body("Not Found").done();
+                    });
+            return http_router;
         }
 
         void run()
         {
-            restinio::run(restinio::on_this_thread<http_server_traits>().port(mmbot_cfg_.http_port).address("localhost"));
+            VLOG_SCOPE_F(loguru::Verbosity_INFO, pretty_function);
+            DVLOG_F(loguru::Verbosity_INFO, "launch http server on port: %d", mmbot_cfg_.http_port);
+            restinio::run(
+                    restinio::on_this_thread<http_server_traits>().port(mmbot_cfg_.http_port).address(
+                            "localhost").request_handler(create_routes()));
         }
 
     private:
         const mmbot::config &mmbot_cfg_;
+
+#ifdef DOCTEST_LIBRARY_INCLUDED
+
+#include <thread>
+#include <csignal>
+        TEST_CASE_CLASS ("test run http_server")
+        {
+            using namespace std::chrono_literals;
+            mmbot::config cfg;
+            cfg.http_port = 8080;
+            http_server server{cfg};
+            std::thread thr(&http_server::run, server);
+            std::this_thread::sleep_for(2s);
+            std::raise(SIGINT);
+            thr.join();
+        }
+
+
+        TEST_CASE_CLASS ("test welcome http_server")
+        {
+            using namespace std::chrono_literals;
+            mmbot::config cfg;
+            cfg.http_port = 8080;
+            http_server server{cfg};
+            std::thread thr(&http_server::run, server);
+            std::this_thread::sleep_for(2s);
+            auto resp = RestClient::get("localhost:8080/");
+            CHECK_EQ(resp.code, 200);
+            std::raise(SIGINT);
+            thr.join();
+        }
+
+#endif
     };
 }
--- src/mmbot.tests.cpp
+++ src/mmbot.tests.cpp
@@ -16,8 +16,4 @@
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
-
-TEST_CASE ("basis")
-{
-    CHECK_EQ(42, 42);
-}
+#include "http/http.server.hpp"
