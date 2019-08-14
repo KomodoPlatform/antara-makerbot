dc019edcccfd695272e4280b18ece81fac89c966
milerius <rmscastle@gmail.com>
2019-08-14 18:39:06 +0200

    feat(dex): add unit tests that launch mm2 binary and test version output

--- src/mm2/mm2.client.hpp
+++ src/mm2/mm2.client.hpp
@@ -16,7 +16,60 @@
 
 #pragma once
 
+#include <future>
+#include <string>
+#include <thread>
+#include <reproc++/reproc.hpp>
+#include <config/config.hpp>
+
 namespace antara::mmbot
 {
 
+    class thread_safe_string_sink
+    {
+    public:
+        thread_safe_string_sink(std::string &out, std::mutex &mutex)
+                : out_(out), mutex_(mutex)
+        {}
+
+        bool operator()(const uint8_t *buffer, unsigned int size)
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            out_.append(reinterpret_cast<const char *>(buffer), size);
+            return true;
+        }
+
+    private:
+        std::string &out_;
+        std::mutex &mutex_;
+    };
+
+    class mm2_client
+    {
+    public:
+        explicit mm2_client(const antara::mmbot::config &cfg) : mmbot_cfg_(cfg)
+        {
+            VLOG_SCOPE_F(loguru::Verbosity_INFO, pretty_function);
+            using namespace std::literals;
+            std::array<std::string, 1> args = {std::filesystem::current_path() / "assets/mm2"};
+            auto path = (std::filesystem::current_path() / "assets/").string();
+            auto ec = background_.start(args, std::addressof(path));
+            if (ec) {
+                VLOG_SCOPE_F(loguru::Verbosity_ERROR, "error: %s", ec.message().c_str());
+            }
+        }
+
+        ~mm2_client()
+        {
+            VLOG_SCOPE_F(loguru::Verbosity_INFO, pretty_function);
+            auto ec = background_.stop(reproc::cleanup::kill, reproc::infinite, reproc::cleanup::terminate, reproc::milliseconds(0));
+            if (ec) {
+                VLOG_SCOPE_F(loguru::Verbosity_ERROR, "error: %s", ec.message().c_str());
+            }
+        }
+
+    private:
+        [[maybe_unused]] const antara::mmbot::config &mmbot_cfg_;
+        reproc::process background_{reproc::cleanup::kill, reproc::infinite, reproc::cleanup::terminate, reproc::milliseconds(0)};
+    };
 }
--- src/mm2/mm2.client.tests.cpp
+++ src/mm2/mm2.client.tests.cpp
@@ -16,9 +16,20 @@
  ******************************************************************************/
 
 #include <doctest/doctest.h>
+#include <restclient-cpp/restclient.h>
 #include "mm2.client.hpp"
+#include "http/http.endpoints.hpp"
 
 namespace antara::mmbot::tests
 {
+    TEST_CASE ("test launch mm2 in background")
+    {
+        auto cfg = load_mmbot_config(std::filesystem::current_path() / "assets", "mmbot_config.json");
+        antara::mmbot::mm2_client mm2(cfg);
 
+        nlohmann::json json_data = {{"method", "version"}, {"userpass", cfg.mm2_rpc_password}};
+        auto resp = RestClient::post(antara::mmbot::mm2_endpoint, "application/json", json_data.dump());
+        CHECK_EQ(200, resp.code);
+        DVLOG_F(loguru::Verbosity_INFO, "body: %s", resp.body.c_str());
+    }
 }
