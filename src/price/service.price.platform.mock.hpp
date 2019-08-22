d2036f0eea1a4e3433cd30884a76ea90c098d835
Daniel Revere <dcprevere@gmail.com>
2019-08-22 18:59:54 +0700

    fix: add missing price service mock file

---
+++ src/price/service.price.platform.mock.hpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+ * Copyright © 2013-2019 The Komodo Platform Developers.                      *
+ *                                                                            *
+ * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * Komodo Platform software, including this file may be copied, modified,     *
+ * propagated or distributed except according to the terms contained in the   *
+ * LICENSE file                                                               *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+#pragma once
+
+#include <doctest/doctest.h>
+#include <doctest/trompeloeil.hpp>
+#include <trompeloeil.hpp>
+
+#include <utils/mmbot_strong_types.hpp>
+#include "service.price.platform.hpp"
+
+namespace antara::mmbot
+{
+    class price_service_platform_mock : public abstract_price_service_platform
+    {
+    public:
+        MAKE_CONST_MOCK1(get_price, st_price(antara::pair));
+        MAKE_MOCK0(enable_price_service_thread, void());
+        MAKE_MOCK1(get_all_price_pairs_of_given_coin, nlohmann::json(const antara::asset &asset));
+        MAKE_MOCK0(fetch_all_price, nlohmann::json());
+        MAKE_MOCK0(get_price_registry, nlohmann::json(), noexcept);
+    };
+}
